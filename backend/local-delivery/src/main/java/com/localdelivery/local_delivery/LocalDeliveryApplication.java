a4a10b215578d0e6a19a6a0a0f7b3c51cd6b3feb
nickhobbes <nickhobbes@icloud.com>
2025-05-12 09:39:04 -0500

    initial backend setup. successful connection to the postgreSQL database.

---
+++ backend/local-delivery/src/main/java/com/localdelivery/local_delivery/LocalDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.localdelivery.local_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDeliveryApplication.class, args);
+	}
+
+}
